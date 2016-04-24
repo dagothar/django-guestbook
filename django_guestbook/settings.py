d080f4ffd4870806ab37693f8a2f4a29ee9a1f72
dagothar <dagothar@gmail.com>
2016-04-24 12:02:45 +0200

    test view

--- django_guestbook/settings.py
+++ django_guestbook/settings.py
@@ -31,6 +31,11 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # project apps
+    'guestbook.apps.GuestbookConfig',
+    # third party apps
+    'widget_tweaks',
+    # Django apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
