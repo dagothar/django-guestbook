6a7eb1d73fd1eeb4335d257e88146c208db76e52
dagothar <dagothar@gmail.com>
2016-04-24 19:53:42 +0200

    jquery to the rescue

--- guestbook/static/js/entry_list.js
+++ guestbook/static/js/entry_list.js
@@ -2,4 +2,24 @@
 
 $(document).ready(function() {
 
+  $('.search').toggle(search_on);
+
+  $('.toggle-search').click(function(e) {
+    e.preventDefault();
+
+    if (search_on) {
+      var search_div = $('.search');
+      search_div.hide();
+
+      $("input[name='filter']").val('');
+      search_div.find("form").submit();
+
+      search_on = false;
+    } else {
+      $('.search').show();
+      search_on = true;
+    }
+
+  });
+
 });
